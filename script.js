8f98a4f1883b209af8497337845e8abff163a628
Thamsanqa Qaba <thamie.qaba@gmail.com>
2024-10-20 12:03:13 +0200

    testing different parameters for java script

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
+    
     // Validation logic goes here
     //let result = 'Invalid Syntax'; // Placeholder for validation result
 
-    if ( validateSyntax() = "pet_" ) {
-        console.log("Valid Syntax");
-    } else {console.log("Invalid Syntax") };
+    if ( result = "pet_" ) {
+        console.log("Valid Syntax")}
 
-    //let result = 'Invalid Syntax'; // Placeholder for validation result
+    let result = 'Invalid Syntax'; // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
