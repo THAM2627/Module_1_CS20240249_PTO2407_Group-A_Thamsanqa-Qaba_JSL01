b4fcdfba4206a1d595eda39944b0df8290f00649
Thamsanqa Qaba <thamie.qaba@gmail.com>
2024-10-20 11:50:38 +0200

    testing if statements and logic

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+    //let result = 'Invalid Syntax'; // Placeholder for validation result
+
+    if ( validateSyntax() = "pet_" ) {
+        console.log("Valid Syntax");
+    } else {console.log("Invalid Syntax") };
+
+    //let result = 'Invalid Syntax'; // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
